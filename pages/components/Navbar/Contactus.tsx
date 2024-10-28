3eace304278ac84f12f9a05cadd5549e00ebb821
RamsaTech <123633437+RamsaTech@users.noreply.github.com>
2024-10-28 08:33:28 +0530

    Update Contactus.tsx

--- pages/components/Navbar/Contactus.tsx
+++ pages/components/Navbar/Contactus.tsx
@@ -2,54 +2,45 @@ import { Dialog, Transition } from '@headlessui/react';
 import { Fragment, useState } from 'react';
 import Link from 'next/link';
 
-
 const Contactusform = () => {
-    let [isOpen, setIsOpen] = useState(false)
-
+    const [isOpen, setIsOpen] = useState(false);
     const [inputValues, setInputValues] = useState({
         input1: '',
         input2: '',
-        input3: ''
+        input3: '',
     });
 
-    const handleChange = (e: { target: { name: string; value: string; }; }) => {
+    const handleChange = (e: { target: { name: string; value: string } }) => {
         const { name, value } = e.target;
-        setInputValues(prevState => ({ ...prevState, [name]: value }));
-    }
+        setInputValues((prevState) => ({ ...prevState, [name]: value }));
+    };
 
     const handleClick = () => {
-        alert(`Name: ${inputValues.input1}, Email-address: ${inputValues.input2}, Message: ${inputValues.input3}`);
-        setIsOpen(false)
-    }
+        alert(
+            `Name: ${inputValues.input1}, Email-address: ${inputValues.input2}, Message: ${inputValues.input3}`
+        );
+        setIsOpen(false);
+    };
 
-    // FORM SUBMIT
-    const handleSubmit = (event: { preventDefault: () => void; }) => {
+    const handleSubmit = (event: { preventDefault: () => void }) => {
         event.preventDefault();
         // handle form submission
-        
     };
 
     const isDisabled = Object.values(inputValues).some((value) => value === '');
 
-
-    const closeModal = () => {
-        setIsOpen(false)
-    }
-
-    const openModal = () => {
-        setIsOpen(true)
-    }
+    const closeModal = () => setIsOpen(false);
+    const openModal = () => setIsOpen(true);
 
     return (
         <>
-            <div className=" inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto md:ml-6 sm:pr-0">
-                {/* <div className='lg:hidden'>
-                    <button type="button" className='bg-navyblue w-full hover:text-white text-white border border-purple font-medium py-2 px-4 rounded' onClick={openModal}>
-                        Contact Us
-                    </button>
-                </div> */}
-                <div className='hidden lg:block'>
-                    <button type="button" className='justify-end text-xl font-semibold bg-transparent py-4 px-6 lg:px-12 navbutton rounded-full hover:bg-navyblue hover:text-white' onClick={openModal}>
+            <div className="inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto md:ml-6 sm:pr-0">
+                <div className="hidden lg:block">
+                    <button
+                        type="button"
+                        className="justify-end text-xl font-semibold bg-transparent py-4 px-6 lg:px-12 navbutton rounded-full hover:bg-navyblue hover:text-white"
+                        onClick={openModal}
+                    >
                         Contact Us
                     </button>
                 </div>
@@ -81,80 +72,104 @@ const Contactusform = () => {
                                 leaveTo="opacity-0 scale-95"
                             >
                                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-
                                     <div className="py-8 lg:py-8 px-4 mx-auto max-w-screen-md">
                                         <div className="flex flex-shrink-0 items-center justify-center">
-                                            <Link href="/" className='text-2xl sm:text-4xl font-semibold text-black text-center'>
+                                            <Link
+                                                href="/"
+                                                className="text-2xl sm:text-4xl font-semibold text-black text-center"
+                                            >
                                                 AgrohaTech Ventures Pvt. Ltd.
                                             </Link>
                                         </div>
-                                        <p className="mb-8 lg:mb-16 mt-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Reg. Address: D1, Temple, Vishvavidlya Puri Tonk Road, 
-Jaipur - 302015
-Rajasthan, India</p>
+                                        <p className="mb-8 lg:mb-16 mt-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+                                            Reg. Address: D1, Temple, Vishvavidlya Puri Tonk Road,
+                                            Jaipur - 302015 Rajasthan, India
+                                        </p>
                                         <p className="mb-8 lg:mb-16 mt-8 font-light text-left text-gray-500 dark:text-gray-400 sm:text-xl">
-
-<b><u>Our Goverment Registrations:</u></b></br>
-CIN: U46512RJ2023PTC087178</br>
-GST: 08AAYCA5780A1ZD</br>
-Call Mobile: +91-9829235735</br>
-Email: info@techdna.com</p>
-                   <p className="mb-8 lg:mb-16 mt-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Submit the form below and we will get back to you...</p>                    
-                                        <form action="#" className="space-y-8" onSubmit={handleSubmit}>
+                                            <b>
+                                                <u>Our Government Registrations:</u>
+                                            </b>
+                                            <br />
+                                            CIN: U46512RJ2023PTC087178
+                                            <br />
+                                            GST: 08AAYCA5780A1ZD
+                                            <br />
+                                            Call Mobile: +91-9829235735
+                                            <br />
+                                            Email: info@techdna.com
+                                        </p>
+                                        <p className="mb-8 lg:mb-16 mt-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+                                            Submit the form below and we will get back to you...
+                                        </p>
+                                        <form
+                                            action="#"
+                                            className="space-y-8"
+                                            onSubmit={handleSubmit}
+                                        >
                                             <div>
-                                                <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Name</label>
+                                                <label
+                                                    htmlFor="text"
+                                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                                >
+                                                    Your Name
+                                                </label>
                                                 <input
                                                     id="text"
-
                                                     name="input1"
                                                     value={inputValues.input1}
                                                     onChange={handleChange}
-
                                                     type="text"
                                                     autoComplete="current-password"
                                                     required
-                                                    className="relative block w-full appearance-none  rounded-md border border-linegrey px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                                    className="relative block w-full appearance-none rounded-md border border-linegrey px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                                     placeholder="Name..."
                                                 />
                                             </div>
                                             <div>
-                                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                                                <label
+                                                    htmlFor="email"
+                                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                                >
+                                                    Your Email
+                                                </label>
                                                 <input
                                                     id="email"
                                                     name="input2"
                                                     value={inputValues.input2}
                                                     onChange={handleChange}
-
                                                     type="email"
                                                     autoComplete="current-password"
                                                     required
-                                                    className="relative block w-full appearance-none  rounded-md border border-linegrey px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                                    className="relative block w-full appearance-none rounded-md border border-linegrey px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                                     placeholder="xyz@email.com"
                                                 />
                                             </div>
                                             <div className="sm:col-span-2">
-                                                <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                                                <label
+                                                    htmlFor="message"
+                                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                                                >
+                                                    Your Message
+                                                </label>
                                                 <textarea
                                                     id="message"
                                                     name="input3"
                                                     value={inputValues.input3}
                                                     onChange={handleChange}
-                                                    className="relative block w-full appearance-none  rounded-md border border-linegrey px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Leave a comment..."></textarea>
+                                                    className="relative block w-full appearance-none rounded-md border border-linegrey px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                                    placeholder="Leave a comment..."
+                                                />
                                             </div>
-                                            <button type="submit"
+                                            <button
+                                                type="submit"
                                                 onClick={handleClick}
                                                 disabled={isDisabled}
-                                                className="py-3 px-5 text-sm disabled:opacity-50 font-medium w-full text-center text-white rounded-lg bg-blue focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
-
+                                                className="py-3 px-5 text-sm disabled:opacity-50 font-medium w-full text-center text-white rounded-lg bg-blue focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                            >
+                                                Send Message
+                                            </button>
                                         </form>
-
                                     </div>
-
-                                    {/* <div className='flex justify-end'>
-                                        <button type="button"
-                                            onClick={closeModal}
-                                            className="py-3 px-5 mt-2 text-sm font-medium w-50 text-center text-white rounded-lg bg-red hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Close</button>
-                                    </div> */}
-
                                 </Dialog.Panel>
                             </Transition.Child>
                         </div>
@@ -162,7 +177,7 @@ Email: info@techdna.com</p>
                 </Dialog>
             </Transition>
         </>
-    )
-}
+    );
+};
 
 export default Contactusform;
